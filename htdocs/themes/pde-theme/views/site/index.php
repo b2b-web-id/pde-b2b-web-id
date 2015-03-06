163dd3828729060d66637b24ffa49d20bc04a5f7
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2015-03-06 18:29:17 +0700

    update: yii

--- htdocs/themes/pde-theme/views/site/index.php
+++ htdocs/themes/pde-theme/views/site/index.php
@@ -2,13 +2,20 @@
 $this->pageTitle=Yii::app()->name.": ".Yii::app()->params['name'];
 ?>
 
-<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit',array(
-    'heading'=>CHtml::encode(Yii::app()->name.": ".Yii::app()->params['name']),
-)); ?>
+<?php
+$this->beginWidget(
+	'bootstrap.widgets.TbHeroUnit',array(
+		'heading'=>CHtml::encode(Yii::app()->name.": ".Yii::app()->params['name']),
+		)
+	);
+?>
 <p>Selamat datang di <?php echo CHtml::encode(Yii::app()->name." (".Yii::app()->params['name'].") "); ?>
 yang dibangun dan dikembangkan oleh <a href='http://www.b2b.web.id'>B2B.Web.ID</a>.</p>
-<?php $this->endWidget(); ?>
+<?php
+$this->endWidget();
+?>
 
-<p>Untuk melihat dan menggunakan data yang ada, Anda harus terdaftar dalam manajemen pengguna kami. Managemen pengguna kami merupakan manajemen pengguna independen.</p>
+<p>Untuk melihat dan menggunakan data yang ada, Anda harus terdaftar dalam manajemen pengguna kami.
+Managemen pengguna kami merupakan manajemen pengguna independen.</p>
 <p>Silakan <?php echo CHtml::link('login',array('site/login')); ?> dengan akun Anda.</p>
 
